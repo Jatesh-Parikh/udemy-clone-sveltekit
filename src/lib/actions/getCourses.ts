85e597c5aa6700655c1ddba3560abef963de0534
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-18 21:03:24 +0530

    add getCourses action

---
+++ src/lib/actions/getCourses.ts
@@ -0,0 +1,58 @@
+import type { Course, CourseWithProgressWithCategory } from "$lib/types";
+import PocketBase from "pocketbase";
+import { getProgress } from "./getProgress";
+
+type getCourses = {
+    userId: string;
+    pb: PocketBase;
+    title: string;
+    categoryId: string;
+};
+
+export const getCourses = async ({
+    pb,
+    categoryId,
+    title,
+    userId
+}: getCourses): Promise<CourseWithProgressWithCategory[]> => {
+    try {
+        const courses = await pb.collection("courses").getFullList<Course>({
+            filter: `isPublished = ${true} && title ?~ "${title}" && category = "${categoryId}" chapters(course).isPublished = ${true} && purchase(course).user = "${userId}"`,
+            expand: 'category,purchase(course),chapters(course)',
+            fields: '*,expand.*,expand.chapters(course).id',
+            sort: '-created',
+        })
+        .catch(() => []);
+
+        const courseWithProgress: CourseWithProgressWithCategory[] = await Promise.all(
+            courses.map(async (course) => {
+                console.log(
+					"🚀 ~ courses.map ~ course.expand?.['purchase(course)']?.length:",
+					course.expand
+				);
+                if (course.expand?.["purchase(course)"]?.length === 0) {
+                    return {
+                        ...course,
+                        progress: null
+                    }
+                }
+
+                const progressPercentage = await getProgress({ courseId: course.id, userId, pb });
+                
+                if (course.imageUrl) {
+                    const imageUrl = pb.files.getUrl(course, course.imageUrl);
+                    course.imageUrl = imageUrl;
+                }
+
+                return {
+                    ...course,
+                    progress: progressPercentage
+                };
+            })
+        );
+
+        return courseWithProgress;
+    } catch (error) {
+        return [];
+    }
+};
