f1324ebee07d7f7362a9a02ab544932b02588a5e
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-18 16:58:34 +0530

    add course and chapter schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -16,3 +16,32 @@ export const registerSchema = z.object({
     message: "Passwords do not match",
     path: ['passwordConfirm']
 });
+
+export const courseSchema = z.object({
+    title: z.string().min(1),
+    description: z.string(),
+    imageUrl: z.string().optional(),
+    price: z.number({ coerce: true }).optional(),
+    isPublished: z.boolean(),
+    category: z.string().optional(),
+});
+
+export const titleSchema = courseSchema.pick({ title: true });
+export const descriptionSchema = courseSchema.pick({ description: true });
+export const categorySchema = courseSchema.pick({ category: true });
+export const priceSchema = courseSchema.pick({ price: true });
+
+export const chapterTitleSchema = z.object({
+    title: z.string().min(1),
+});
+
+export const chapterDescriptionSchema = z.object({
+    description: z.string()
+});
+
+export const chapterAccessSchema = z.object({
+    isFree: z.boolean(),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
+export type CourseSchema = z.infer<typeof courseSchema>;
