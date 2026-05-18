016d1802f6bd3a3079d1d4dbbb721ba4a51f7464
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-18 21:30:10 +0530

    add root layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import '@fontsource/noto-sans';
+	import '@fontsource/noto-sans/100.css';
+	import '@fontsource/noto-sans/200.css';
+	import '@fontsource/noto-sans/300.css';
+	import '@fontsource/noto-sans/400.css';
+	import '@fontsource/noto-sans/500.css';
+	import '@fontsource/noto-sans/600.css';
+	import '@fontsource/noto-sans/700.css';
+	import '@fontsource/noto-sans/800.css';
+	import '@fontsource/noto-sans/900.css';
+	import 'quill/dist/quill.snow.css';
 
-	let { children } = $props();
+	import "../app.css";
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<ModeWatcher defaultMode="light" />
+<Toaster richColors closeButton theme="light" />
+<div class="h-screen">
+	<slot />
+</div>
