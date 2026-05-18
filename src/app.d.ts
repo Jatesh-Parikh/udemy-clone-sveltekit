394edf2044a55f54fc9d6795079850b454131a4a
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-18 10:04:36 +0530

    add global types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,28 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import Pocketbase, { type AuthModel } from "pocketbase";
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: Pocketbase;
+			user: AuthModel | undefined;
+		}
 		// interface PageData {}
-		// interface PageState {}
 		// interface Platform {}
+	}
+	declare type Item = import('svelte-dnd-action').Item;
+	declare type DndEvent<ItemType = Item> = import('svelte-dnd-action').DndEvent<ItemType>;
+	declare namespace svelteHTML {
+		interface HTMLAttributes<T> {
+			'on:consider'?: (
+				event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }
+			) => void;
+			'on:finalize'?: (
+				event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }
+			) => void;
+		}
 	}
 }
 
