d5d3c5bfd68e36730a04c607918bbbe6a8c4e229
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-19 21:51:06 +0530

    add isPublishedBadge component

---
+++ src/lib/components/IsPublishedBadge.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { Badge } from "$lib/components/ui/badge";
+    import { cn } from "$lib/utils";
+    export let value: boolean;
+</script>
+
+<Badge
+    class={cn(
+        'bg-slate-500', {
+            'bg-sky-700': value,
+        }
+    )}
+>
+    {#if value}
+        Published
+    {:else}
+        Draft
+    {/if}
+</Badge>
