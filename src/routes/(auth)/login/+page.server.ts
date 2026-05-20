77f56cee92ec9db2211dca8af414f97805609b2b
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-20 20:34:59 +0530

    add login server load file

---
+++ src/routes/(auth)/login/+page.server.ts
@@ -0,0 +1,31 @@
+import { message, superValidate } from "sveltekit-superforms/server";
+import { loginSchema } from "$lib/schema";
+import { zod } from "sveltekit-superforms/adapters";
+import { fail, redirect } from "@sveltejs/kit";
+import type { ClientResponseError } from "pocketbase";
+
+export const load = async () => {
+    return {
+        form: await superValidate(zod(loginSchema))
+    }
+} 
+
+export const actions = {
+    default: async (event) => {
+        const { locals: { pb } } = event;
+        const form = await superValidate(event, zod(loginSchema));
+        if (!form.valid) {
+            return fail(400, { form });
+        }
+
+        try {
+            await pb.collection('users').authWithPassword(form.data.email, form.data.password);
+        } catch (e) {
+            console.log("Error:", e);
+            const { status } = e as ClientResponseError;
+
+            return message(form, { status, message: "An error occurred" });
+        }
+        redirect(303, '/');
+    }
+}
