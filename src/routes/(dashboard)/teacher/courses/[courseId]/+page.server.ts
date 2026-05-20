d66c581b7563b259351b1c9caa730fb82f60bcab
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-20 22:05:43 +0530

    add updateDescription action

--- src/routes/(dashboard)/teacher/courses/[courseId]/+page.server.ts
+++ src/routes/(dashboard)/teacher/courses/[courseId]/+page.server.ts
@@ -89,4 +89,22 @@ export const actions = {
 			return message(form, errorMessage, { status: 400 });
 		}
 	},
+    updateDescription: async (event) => {
+		const { locals: { pb }, params } = event;
+		const { courseId } = params;
+
+		const form = await superValidate(event, zod(descriptionSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		try {
+			await pb.collection('courses').update(courseId, form.data);
+			return message(form, 'Successfully updated course description');
+		} catch (e) {
+			const { message: errorMessage } = e as ClientResponseError;
+
+			return message(form, errorMessage, { status: 400 });
+		}
+	},
 };
