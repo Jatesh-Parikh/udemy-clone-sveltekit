e3a68f68f02d2d5ae1ae2574f3a45c35f17f7b9c
Jatesh Parikh <jateshparikh97@gmail.com>
2026-05-21 21:39:32 +0530

    add dynamic chapter server load function

---
+++ src/routes/(dashboard)/teacher/courses/[courseId]/chapters/[chapterId]/+page.server.ts
@@ -0,0 +1,45 @@
+import { chapterAccessSchema, chapterDescriptionSchema, chapterTitleSchema } from '$lib/schema.js';
+import type { Chapter, Course, MuxData } from '$lib/types';
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { ClientResponseError } from 'pocketbase';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import Mux from '@mux/mux-node';
+import { MUX_TOKEN_ID, MUX_TOKEN_SECRET } from '$env/static/private';
+import { zod } from 'sveltekit-superforms/adapters';
+const mux = new Mux({ tokenId: MUX_TOKEN_ID, tokenSecret: MUX_TOKEN_SECRET });
+
+export const load = async ({ params, locals: { pb } }) => {
+	const chapterId = params.chapterId;
+	async function getChapter() {
+		try {
+			const chapter = await pb.collection('chapters').getOne<Chapter>(chapterId, {
+				expand: 'muxData(chapterId)'
+			});
+			if (chapter.videoUrl) {
+				const videoUrl = pb.files.getUrl(chapter, chapter.videoUrl);
+				chapter.videoUrl = videoUrl;
+			}
+			return chapter;
+		} catch (e) {
+			const { status } = e as ClientResponseError;
+
+			if (status === 404) error(404, 'Course does not exist');
+			redirect(308, '/');
+		}
+	}
+	const chapter = await getChapter();
+	const chapterTitleForm = await superValidate(chapter, zod(chapterTitleSchema));
+	const chapterDescriptionForm = await superValidate(chapter, zod(chapterDescriptionSchema));
+	const chapterAccessForm = await superValidate(chapter, zod(chapterAccessSchema));
+
+	return {
+		chapter,
+		chapterTitleForm,
+		chapterDescriptionForm,
+		chapterAccessForm
+	};
+};
+
+export const actions = {
+    
+};
